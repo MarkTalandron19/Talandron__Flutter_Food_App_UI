b99af730decf9da81034ce38667e9204a018debb
Mark Allen Talandron <markallen.talandron.20@usjr.edu.ph>
2023-02-07 21:58:47 +0800

    fixed the appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -11,9 +11,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
           elevation: 0,
+          floating: true,
           title: Column(children: [
             Text(
               "Delivery to".toUpperCase(),
@@ -33,105 +36,77 @@ class HomeScreen extends StatelessWidget {
                 ))
           ],
         ),
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              elevation: 0,
-              floating: true,
-              title: Column(children: [
-                Text(
-                  "Delivery to".toUpperCase(),
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(color: kActiveColor),
-                ),
-                const Text("San Francisco",
-                    style: TextStyle(color: Colors.black))
-              ]),
-              actions: [
-                TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Filter",
-                      style: TextStyle(color: Colors.black),
-                    ))
-              ],
+        SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+          sliver: SliverToBoxAdapter(
+            child: ImageCarousel(),
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.all(defaultPadding),
+          sliver: SliverToBoxAdapter(
+            child: SectionTitle(
+              title: "Featured Partners",
+              press: () {},
             ),
-            SliverPadding(
-              padding: EdgeInsets.symmetric(horizontal: defaultPadding),
-              sliver: SliverToBoxAdapter(
-                child: ImageCarousel(),
-              ),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.all(defaultPadding),
-              sliver: SliverToBoxAdapter(
-                child: SectionTitle(
-                  title: "Featured Partners",
-                  press: () {},
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                demoMediumCardData.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(left: defaultPadding),
+                  child: RestaurantInfoMediumCard(
+                      title: demoMediumCardData[index]['name'],
+                      location: demoMediumCardData[index]['location'],
+                      image: demoMediumCardData[index]['image'],
+                      deliveryTime: demoMediumCardData[index]['delivertTime'],
+                      rating: demoMediumCardData[index]['rating'],
+                      press: () {}),
                 ),
               ),
             ),
-            SliverToBoxAdapter(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.generate(
-                    demoMediumCardData.length,
-                    (index) => Padding(
-                      padding: const EdgeInsets.only(left: defaultPadding),
-                      child: RestaurantInfoMediumCard(
-                          title: demoMediumCardData[index]['name'],
-                          location: demoMediumCardData[index]['location'],
-                          image: demoMediumCardData[index]['images'],
-                          deliveryTime: demoMediumCardData[index]
-                              ['delivertTime'],
-                          rating: demoMediumCardData[index]['rating'],
-                          press: () {}),
-                    ),
-                  ),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.all(defaultPadding),
+          sliver: SliverToBoxAdapter(
+            child: Image.asset("assets/images/Banner.png"),
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.all(defaultPadding),
+          sliver: SliverToBoxAdapter(
+            child: SectionTitle(
+              title: "Best Pick",
+              press: () {},
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                demoMediumCardData.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(left: defaultPadding),
+                  child: RestaurantInfoMediumCard(
+                      title: demoMediumCardData[index]['name'],
+                      location: demoMediumCardData[index]['location'],
+                      image: demoMediumCardData[index]['image'],
+                      deliveryTime: demoMediumCardData[index]['delivertTime'],
+                      rating: demoMediumCardData[index]['rating'],
+                      press: () {}),
                 ),
               ),
             ),
-            SliverPadding(
-              padding: const EdgeInsets.all(defaultPadding),
-              sliver: SliverToBoxAdapter(
-                child: Image.asset("assets/images/Banner.png"),
-              ),
-            ),
-            SliverPadding(
-              padding: EdgeInsets.all(defaultPadding),
-              sliver: SliverToBoxAdapter(
-                child: SectionTitle(
-                  title: "Best Pick",
-                  press: () {},
-                ),
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.generate(
-                    demoMediumCardData.length,
-                    (index) => Padding(
-                      padding: const EdgeInsets.only(left: defaultPadding),
-                      child: RestaurantInfoMediumCard(
-                          title: demoMediumCardData[index]['name'],
-                          location: demoMediumCardData[index]['location'],
-                          image: demoMediumCardData[index]['images'],
-                          deliveryTime: demoMediumCardData[index]
-                              ['delivertTime'],
-                          rating: demoMediumCardData[index]['rating'],
-                          press: () {}),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ));
+          ),
+        ),
+      ],
+    ));
   }
 }
